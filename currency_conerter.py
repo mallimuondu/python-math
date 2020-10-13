ec83206a06f34ce377913504206805e367d52377
mallimuondu <mallimuondu@gmail.com>
2020-10-13 13:03:23 +0300

    added code

---
+++ currency_conerter.py
@@ -0,0 +1,5 @@
+a = int(input('insert dollers$ to ksh$:'))
+
+b = 108.59
+c = a*b
+print('your ammount  = ',c)
---
+++ ksh_dollers.py
@@ -0,0 +1,5 @@
+a = int(input('insert ksh$ to dollers$:'))
+
+b = 0.0092
+c = a*b
+print('your ammount  = 'c)
